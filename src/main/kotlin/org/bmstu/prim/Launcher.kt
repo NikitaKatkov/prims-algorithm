174a99d82fdb919a69394a324d97856dd83296dc
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2019-11-28 20:31:02 +0300

    fixed uri in file reader

--- src/main/kotlin/org/bmstu/prim/Launcher.kt
+++ src/main/kotlin/org/bmstu/prim/Launcher.kt
@@ -3,13 +3,15 @@ package org.bmstu.prim
 import org.bmstu.prim.impl.core.CoroutineBasedPrim
 import org.bmstu.prim.impl.core.Solver
 import org.bmstu.prim.impl.input.ConsoleInputReader
+import org.bmstu.prim.impl.view.ConsoleViewer
 import org.bmstu.prim.impl.view.FileWriter
 
 fun main() {
     Solver(
         ConsoleInputReader(),
-        CoroutineBasedPrim(4),
-        FileWriter("result.txt")
+        CoroutineBasedPrim(1),
+        FileWriter("result.txt"),
+        ConsoleViewer()
     ).solve()
 }
 
