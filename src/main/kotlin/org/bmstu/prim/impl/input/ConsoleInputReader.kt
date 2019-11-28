dfc8e47160051306861a906ad0bdfe8e6def9ee1
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2019-11-28 20:31:02 +0300

    fixed uri in file reader
    (cherry picked from commit 174a99d82fdb919a69394a324d97856dd83296dc)

--- src/main/kotlin/org/bmstu/prim/impl/input/ConsoleInputReader.kt
+++ src/main/kotlin/org/bmstu/prim/impl/input/ConsoleInputReader.kt
@@ -2,12 +2,13 @@ package org.bmstu.prim.impl.input
 
 import org.bmstu.prim.api.graph.Graph
 import org.bmstu.prim.api.input.InputReader
-import java.net.URI
+import java.io.File
 
 class ConsoleInputReader : InputReader {
     override fun parseInput(): Graph {
         println("Enter file path:")
-        val path = URI(readLine()!!)
-        return FileInputReader(path).parseInput()
+        val path = readLine()!!
+        val pathUri = File(path).toURI()
+        return FileInputReader(pathUri).parseInput()
     }
 }
