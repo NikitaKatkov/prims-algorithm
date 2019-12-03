84d1c36575e1d892965c68b73a34fc58f444ac42
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2019-12-03 21:59:45 +0300

    convenient but ugly way to launch from main

--- src/main/kotlin/org/bmstu/prim/Launcher.kt
+++ src/main/kotlin/org/bmstu/prim/Launcher.kt
@@ -2,16 +2,23 @@ package org.bmstu.prim
 
 import org.bmstu.prim.impl.core.CoroutineBasedPrim
 import org.bmstu.prim.impl.core.Solver
-import org.bmstu.prim.impl.input.ConsoleInputReader
+import org.bmstu.prim.impl.input.FileInputReader
 import org.bmstu.prim.impl.view.ConsoleViewer
 import org.bmstu.prim.impl.view.FileWriter
+import java.io.File
 
 fun main() {
     Solver(
-        ConsoleInputReader(),
-        CoroutineBasedPrim(1),
+        FileInputReader(setupWithPredefinedInput().toURI()),
+        CoroutineBasedPrim(4),
         FileWriter("result.txt"),
         ConsoleViewer()
     ).solve()
+}
+
+fun setupWithPredefinedInput(): File {
+    println("Type graph size (100, 1000, 3000): ")
+    val size = readLine()!!.toInt()
+    return File("/Users/nikita_katkov/IdeaProjects/bmstu/primsalgorithm/src/test/resources/largeGraphs/$size.txt")
 }
 
